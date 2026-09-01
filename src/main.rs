37e8fd0a9d2b13bd55a1cc2dd186061938d110a8
Leonardo Gagliardoni <feuergas@gmail.com>
2026-09-01 14:05:00 +0200

    Solution of Powerful array

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,24 @@
-// use std::collections::BTreeSet;
-use std::{collections::BTreeSet, io::Read, ops::Bound::Included};
+use std::io::Read;
 
 struct Solution {}
 
-#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
-struct Frog {
-    tongue: u64,
-    pos: u64,
-    eaten_mosquitoes: u64,
-    index: usize,
-}
-
-#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
-struct Mosquito {
-    pos: u64,
-    size: u64,
-    index: usize,
-}
-
-fn get_input(it: &mut std::str::SplitWhitespace<'_>) -> (Vec<Frog>, Vec<Mosquito>) {
+fn get_input(it: &mut std::str::SplitWhitespace<'_>) -> (Vec<u64>, Vec<(u64, u64)>) {
     let n: u64 = it.next().unwrap().parse().unwrap();
-    let m: u64 = it.next().unwrap().parse().unwrap();
+    let t: u64 = it.next().unwrap().parse().unwrap();
 
-    let frogs: Vec<Frog> = (0..n)
-        .map(|idx| {
-            let pos: u64 = it.next().unwrap().parse().unwrap();
-            let tongue: u64 = it.next().unwrap().parse().unwrap();
-            Frog {
-                pos,
-                tongue: tongue + pos,
-                eaten_mosquitoes: 0,
-                index: idx as usize,
-            }
+    let arr: Vec<u64> = (0..n)
+        .map(|_| it.next().unwrap().parse().unwrap())
+        .collect();
+
+    let queries: Vec<(u64, u64)> = (0..t)
+        .map(|_| {
+            let l: u64 = it.next().unwrap().parse().unwrap();
+            let r: u64 = it.next().unwrap().parse().unwrap();
+            (l - 1, r - 1) // Convert to 0-based indexing
         })
         .collect();
 
-    let mosquitoes: Vec<Mosquito> = (0..m)
-        .map(|idx| {
-            let pos: u64 = it.next().unwrap().parse().unwrap();
-            let size: u64 = it.next().unwrap().parse().unwrap();
-            Mosquito {
-                pos,
-                size,
-                index: idx as usize,
-            }
-        })
-        .collect();
-
-    (frogs, mosquitoes)
+    (arr, queries)
 }
 
 fn main() {
@@ -59,78 +30,78 @@ fn main() {
     let t: u64 = 1;
 
     for _ in 0..t {
-        let (frogs, mosquitoes) = get_input(&mut it);
+        let (arr, queries) = get_input(&mut it);
 
-        let sol: Vec<(u64, u64)> = Solution::frogs_and_mosquitoes(frogs, mosquitoes);
+        let sol: Vec<u64> = Solution::powerful_array(arr, queries);
 
-        for val in sol.iter() {
-            println!("{} {}", val.0, val.1);
+        for ans in sol {
+            println!("{}", ans);
         }
     }
 }
 
 impl Solution {
-    pub fn frogs_and_mosquitoes(frogs: Vec<Frog>, mosquitoes: Vec<Mosquito>) -> Vec<(u64, u64)> {
-        let mut frogs: BTreeSet<Frog> = frogs.into_iter().collect();
-        let mut landed_mosquitoes: BTreeSet<Mosquito> = BTreeSet::new();
+    pub fn powerful_array(arr: Vec<u64>, queries: Vec<(u64, u64)>) -> Vec<u64> {
+        const RANGE: usize = 1_000_001;
 
-        for mosq in mosquitoes {
-            let frog: Option<Frog> = frogs
-                .range(
-                    Frog {
-                        pos: 0,
-                        tongue: mosq.pos,
-                        eaten_mosquitoes: 0,
-                        index: 0,
-                    }..,
-                )
-                .next()
-                .filter(|frog: &&Frog| frog.pos <= mosq.pos)
-                .cloned();
+        let mut sorted_queries: Vec<(u64, u64, usize)> = queries
+            .iter()
+            .enumerate()
+            .map(|(i, &q)| (q.0, q.1, i))
+            .collect();
 
-            if let Some(frog) = frog {
-                // Frog eats the mosquito
-                frogs.remove(&frog);
-                let mut updated_frog: Frog = frog.clone();
-                updated_frog.tongue += mosq.size;
-                updated_frog.eaten_mosquitoes += 1;
+        let sqrt_n: u64 = (arr.len() as f64).sqrt() as u64 + 1;
+        sorted_queries.sort_by_key(|&(l, r, _)| (l / sqrt_n, r));
 
-                // Frog keeps eating mosquitoes that are within its tongue range
-                while let Some(l_mosq) = landed_mosquitoes
-                    .range((
-                        Included(Mosquito {
-                            pos: updated_frog.pos,
-                            size: 0,
-                            index: 0,
-                        }),
-                        Included(Mosquito {
-                            pos: updated_frog.tongue,
-                            size: u64::MAX,
-                            index: usize::MAX,
-                        }),
-                    ))
-                    .next()
-                    .cloned()
-                {
-                    landed_mosquitoes.remove(&l_mosq);
-                    updated_frog.tongue += l_mosq.size;
-                    updated_frog.eaten_mosquitoes += 1;
-                }
+        let mut answers: Vec<u64> = Vec::with_capacity(queries.len());
+        let mut counters: Vec<u64> = vec![0; RANGE];
 
-                // Insert the updated frog back into the set
-                frogs.insert(updated_frog);
-            } else {
-                landed_mosquitoes.insert(mosq);
+        let mut cur_l: u64 = 0;
+        let mut cur_r: u64 = 0;
+        let mut cur_ans: u64 = arr[0];
+        counters[arr[0] as usize] = 1;
+
+        for &(l, r, _) in &sorted_queries {
+            let mut add = |i: u64| {
+                let x: u64 = arr[i as usize];
+                counters[x as usize] += 1;
+                cur_ans += (2 * counters[x as usize] - 1) * x;
+            };
+
+            while cur_l > l {
+                cur_l -= 1;
+                add(cur_l);
             }
+
+            while cur_r < r {
+                cur_r += 1;
+                add(cur_r);
+            }
+
+            let mut remove = |i: u64| {
+                let x: u64 = arr[i as usize];
+                cur_ans -= (2 * counters[x as usize] - 1) * x;
+                counters[x as usize] -= 1;
+            };
+
+            while cur_l < l {
+                remove(cur_l);
+                cur_l += 1;
+            }
+
+            while cur_r > r {
+                remove(cur_r);
+                cur_r -= 1;
+            }
+
+            answers.push(cur_ans);
         }
 
-        let mut frogs: Vec<Frog> = frogs.into_iter().collect();
+        let mut unpermuted_answers: Vec<u64> = vec![0; queries.len()];
+        for (&ans, (_, _, i)) in answers.iter().zip(sorted_queries) {
+            unpermuted_answers[i] = ans;
+        }
 
-        frogs.sort_by_key(|frog: &Frog| frog.index);
-
-        frogs
-            .into_iter()
-            .map(|frog: Frog| (frog.eaten_mosquitoes, frog.tongue - frog.pos))
-            .collect()
+        unpermuted_answers
     }
 }
