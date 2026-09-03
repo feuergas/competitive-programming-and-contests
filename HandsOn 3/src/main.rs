ded280e0d2454eccb42f86eb3847ee41b31ccc4f
Leonardo Gagliardoni <feuergas@gmail.com>
2026-09-03 16:59:19 +0200

    Testing code

--- HandsOn 3/src/main.rs
+++ HandsOn 3/src/main.rs
@@ -1,44 +1,17 @@
-// use handson_3::*;
+use handson_3::length_of_lis;
 use std::fs;
 
 fn main() {
-    let t: i32 = 5;
+    let t: i32 = 11;
 
     for index in 0..t {
-        let (n, d, mut cities) = read_input(FOLDER_PATH, index);
+        let mut topics = read_input(FOLDER_PATH, index);
 
-        let prefix_sum = |arr: &Vec<i32>| {
-            let mut prefix = Vec::with_capacity(arr.len() + 1);
-            prefix.push(0);
-            for &x in arr {
-                prefix.push(prefix.last().unwrap() + x);
-            }
-            prefix
-        };
+        topics.sort_by_key(|&(b, d)| (b, -d));
 
-        // Convert each city to its prefix sum array
-        cities.iter_mut().for_each(|city| {
-            *city = prefix_sum(city);
-        });
+        let difficulty = topics.iter().map(|&(_, d)| d).collect();
 
-        // store in best[i][j] the most value we can get after having visited the first i cities in j days
-        let mut best = vec![vec![0; d+1]; n+1];
-
-        // When i = 0 we have visited no cities, hence the value is always 0
-        // When j = 0 we have spent no days in any city, hence the value is always 0
-        for i in 1..=n {
-            for j in 1..=d {
-                for k in 0..=d {
-                    if j < k {
-                        break;
-                    }
-                    best[i][j] = best[i][j].max(best[i - 1][j - k] + cities[i - 1][k]);
-                }
-            }
-        }
-
-        let mut answers: Vec<i32> = Vec::new();
-        answers.push(best[n][d]);
+        let answers: Vec<i32> = vec![length_of_lis(difficulty)];
 
         let correct_answers = read_output(FOLDER_PATH, index);
         assert_eq!(answers, correct_answers);
@@ -46,25 +19,22 @@ fn main() {
     }
 }
 
-const FOLDER_PATH: &str = "Testset_handson3_p1"; // Set path to input and output folder location
+const FOLDER_PATH: &str = "Testset_handson3_p2"; // Set path to input and output folder location
 
-fn read_input(folder_path: &str, index: i32) -> (usize, usize, Vec<Vec<i32>>) {
+fn read_input(folder_path: &str, index: i32) -> Vec<(i32, i32)> {
     let input_name: String = format!("input{index}.txt");
     let input: String = fs::read_to_string(format!("{folder_path}/{input_name}")).unwrap();
     let mut it: std::str::SplitWhitespace<'_> = input.split_whitespace();
 
     let n: usize = it.next().unwrap().parse().unwrap();
-    let d: usize = it.next().unwrap().parse().unwrap();
 
-    let cities = (0..n)
+    (0..n)
         .map(|_| {
-            (0..d)
-                .map(|_| it.next().unwrap().parse().unwrap())
-                .collect()
+            let b = it.next().unwrap().parse().unwrap();
+            let d = it.next().unwrap().parse().unwrap();
+            (b, d)
         })
-        .collect();
-
-    (n, d, cities)
+        .collect()
 }
 
 fn read_output(folder_path: &str, index: i32) -> Vec<i32> {
