4f3d574684f6b008ff6f332e185bbfb222442e63
Leonardo Gagliardoni <feuergas@gmail.com>
2026-09-04 22:51:21 +0200

    Solution to Frogs and mosquitoes

--- Frogs And Mosquitoes/src/main.rs
+++ Frogs And Mosquitoes/src/main.rs
@@ -1,47 +1,23 @@
 // use std::collections::BTreeSet;
-use std::{collections::BTreeSet, io::Read, ops::Bound::Included};
+use std::{collections::BTreeSet, io::Read};
 
-#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
-struct Frog {
-    tongue: u64,
-    pos: u64,
-    eaten_mosquitoes: u64,
-    index: usize,
-}
+fn get_input(it: &mut std::str::SplitWhitespace<'_>) -> (Vec<(u64, u64)>, Vec<(u64, u64)>) {
+    let n: usize = it.next().unwrap().parse().unwrap();
+    let m: usize = it.next().unwrap().parse().unwrap();
 
-#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
-struct Mosquito {
-    pos: u64,
-    size: u64,
-    index: usize,
-}
-
-fn get_input(it: &mut std::str::SplitWhitespace<'_>) -> (Vec<Frog>, Vec<Mosquito>) {
-    let n: u64 = it.next().unwrap().parse().unwrap();
-    let m: u64 = it.next().unwrap().parse().unwrap();
-
-    let frogs: Vec<Frog> = (0..n)
-        .map(|idx| {
+    let frogs: Vec<(u64, u64)> = (0..n)
+        .map(|_| {
             let pos: u64 = it.next().unwrap().parse().unwrap();
             let tongue: u64 = it.next().unwrap().parse().unwrap();
-            Frog {
-                pos,
-                tongue: tongue + pos,
-                eaten_mosquitoes: 0,
-                index: idx as usize,
-            }
+            (pos, tongue)
         })
         .collect();
 
-    let mosquitoes: Vec<Mosquito> = (0..m)
-        .map(|idx| {
+    let mosquitoes: Vec<(u64, u64)> = (0..m)
+        .map(|_| {
             let pos: u64 = it.next().unwrap().parse().unwrap();
             let size: u64 = it.next().unwrap().parse().unwrap();
-            Mosquito {
-                pos,
-                size,
-                index: idx as usize,
-            }
+            (pos, size)
         })
         .collect();
 
@@ -61,7 +37,7 @@ fn main() {
 
         let sol: Vec<(u64, u64)> = Solution::frogs_and_mosquitoes(frogs, mosquitoes);
 
-        for val in sol.iter() {
+        for val in sol {
             println!("{} {}", val.0, val.1);
         }
     }
@@ -70,67 +46,90 @@ fn main() {
 struct Solution {}
 
 impl Solution {
-    pub fn frogs_and_mosquitoes(frogs: Vec<Frog>, mosquitoes: Vec<Mosquito>) -> Vec<(u64, u64)> {
-        let mut frogs: BTreeSet<Frog> = frogs.into_iter().collect();
-        let mut landed_mosquitoes: BTreeSet<Mosquito> = BTreeSet::new();
-
-        for mosq in mosquitoes {
-            let frog: Option<Frog> = frogs
-                .range(
-                    Frog {
-                        pos: 0,
-                        tongue: mosq.pos,
-                        eaten_mosquitoes: 0,
-                        index: 0,
-                    }..,
-                )
-                .next()
-                .filter(|frog: &&Frog| frog.pos <= mosq.pos)
-                .cloned();
-
-            if let Some(frog) = frog {
-                // Frog eats the mosquito
-                frogs.remove(&frog);
-                let mut updated_frog: Frog = frog.clone();
-                updated_frog.tongue += mosq.size;
-                updated_frog.eaten_mosquitoes += 1;
-
-                // Frog keeps eating mosquitoes that are within its tongue range
-                while let Some(l_mosq) = landed_mosquitoes
-                    .range((
-                        Included(Mosquito {
-                            pos: updated_frog.pos,
-                            size: 0,
-                            index: 0,
-                        }),
-                        Included(Mosquito {
-                            pos: updated_frog.tongue,
-                            size: u64::MAX,
-                            index: usize::MAX,
-                        }),
-                    ))
-                    .next()
-                    .cloned()
-                {
-                    landed_mosquitoes.remove(&l_mosq);
-                    updated_frog.tongue += l_mosq.size;
-                    updated_frog.eaten_mosquitoes += 1;
-                }
-
-                // Insert the updated frog back into the set
-                frogs.insert(updated_frog);
+    pub fn frogs_and_mosquitoes(
+        frogs: Vec<(u64, u64)>,
+        mosquitoes: Vec<(u64, u64)>,
+    ) -> Vec<(u64, u64)> {
+        let mut answers: Vec<(u64, u64)> = frogs.iter().map(|&(_, tongue)| (0, tongue)).collect();
+        // store the right enpoints of the eating range for each frog
+        let mut frog_ranges: Vec<(u64, Option<usize>)> = frogs
+            .iter()
+            .enumerate()
+            .map(|(idx, &(pos, tongue))| (pos + tongue, Some(idx)))
+            .collect();
+        // sort the right endpoints by increasing left endpoints
+        frog_ranges.sort_by_key(|&(_, idx)| frogs[idx.unwrap()]);
+        // remove the right endpoints of the dominated frogs
+        let mut max_range = u64::MIN;
+        frog_ranges.retain(|&(range, _)| {
+            if range <= max_range {
+                false
             } else {
-                landed_mosquitoes.insert(mosq);
+                max_range = range;
+                true
+            }
+        });
+        // convert frog ranges to ordered set
+        let mut frog_ranges: BTreeSet<(u64, Option<usize>)> = frog_ranges.into_iter().collect();
+        /* SEEMS WORKING TILL HERE */
+        // store yet to be eaten mosquitoes in an ordered set
+        let mut landed_mosquitoes: BTreeSet<(u64, Option<usize>)> = BTreeSet::new();
+
+        for (curr_idx, &(curr_pos, curr_size)) in mosquitoes.iter().enumerate() {
+            let frog_query = frog_ranges
+                .range((curr_pos, None)..)
+                .next()
+                .clone();
+
+            let mut ate_mosquito = false;
+
+            if let Some(&(mut curr_range, idx)) = frog_query {
+                let idx = idx.unwrap();
+
+                if frogs[idx].0 <= curr_pos {
+                    frog_ranges.remove(&(curr_range, Some(idx)));
+                    ate_mosquito = true;
+                    let old_range = curr_range;
+                    // eat current mosquito
+                    answers[idx].0 += 1;
+                    answers[idx].1 += curr_size;
+                    curr_range += curr_size;
+
+                    let frog_pos = frogs[idx].0;
+                    // keep eating landed mosquitoes in reach
+                    while let Some(new_mosq) = landed_mosquitoes
+                        .range((frog_pos, None)..(curr_range + 1, None))
+                        .next()
+                        .copied()
+                    {
+                        landed_mosquitoes.remove(&new_mosq);
+                        let new_mosq_idx = new_mosq.1.unwrap();
+                        // eat new mosquito
+                        answers[idx].0 += 1;
+                        answers[idx].1 += mosquitoes[new_mosq_idx].1;
+                        curr_range += mosquitoes[new_mosq_idx].1;
+                    }
+
+                    // remove newly dominated frogs
+                    while let Some(sub_frog) = frog_ranges
+                        .range((old_range, None)..(curr_range + 1, None))
+                        .next()
+                        .copied()
+                    {
+                        frog_ranges.remove(&sub_frog);
+                    }
+
+                    // add back the eating frog
+                    frog_ranges.insert((frog_pos + answers[idx].1, Some(idx)));
+                }
+            }
+
+            // If no frog could eat it, add it to landed mosquitoes
+            if !ate_mosquito {
+                landed_mosquitoes.insert((curr_pos, Some(curr_idx)));
             }
         }
 
-        let mut frogs: Vec<Frog> = frogs.into_iter().collect();
-
-        frogs.sort_by_key(|frog: &Frog| frog.index);
-
-        frogs
-            .into_iter()
-            .map(|frog: Frog| (frog.eaten_mosquitoes, frog.tongue - frog.pos))
-            .collect()
+        answers
     }
 }
